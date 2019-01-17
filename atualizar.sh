58acb276300652cc3a11ab6a021a688c6454106d
yago Aparecido dos Santos Silva <43183773+YagogesKRYEG@users.noreply.github.com>
2019-01-17 16:34:05 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update 
  echo 
  echo  listas atualizadas
+ sleep 5
+ clear
  
  echo atualizando os softwares
 echo
 	sudo apt -y upgrade
  echo 
  echo  softwares atualizados
+ sleep 5
+ clear
  
  echo atualizando o Kernel
 echo
 	sudo apt -y dist-upgrade
  echo 
  echo  Kernel atualizados
+ sleep 5
+ clear
  
  echo removendo softwares
 echo
 	sudo apt -y autoremove
  echo 
  echo  softwares removidos
+ sleep 5
+ clear
  
   echo limpando cache
 echo
 	sudo apt -y autoclean
  echo 
  echo  Cache limpo 
+ sleep 5
+ clear
